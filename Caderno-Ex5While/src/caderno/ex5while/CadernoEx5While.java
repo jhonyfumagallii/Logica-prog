cdce32bc6e622ae0d0b6eb29f6c8cb68a57c737b
Jhony_Fumagalli <jhonyfumagalli4@gmail.com>
2026-07-16 11:35:33 -0300

    Add files via upload

---
+++ Caderno-Ex5While/src/caderno/ex5while/CadernoEx5While.java
@@ -0,0 +1,37 @@
+package caderno.ex5while;
+
+import java.util.Scanner;
+
+public class CadernoEx5While {
+
+    public static void main(String[] args) {
+        Scanner teclado = new Scanner(System.in);
+        double cont = 0, media, acum = 0, nota;
+        String nome;
+        while (cont <= 35) {
+            cont++;
+            System.out.println("\nInsira o nome");
+            nome = teclado.nextLine();
+            System.out.println("Insira a nota 1");
+            nota = teclado.nextDouble();
+            acum = acum + nota;
+            System.out.println("Insira a nota 2");
+            nota = teclado.nextDouble();
+            acum = acum + nota;
+            System.out.println("Insira a nota 3");
+            nota = teclado.nextDouble();
+            acum = acum + nota;
+            System.out.println("Insira a nota 4");
+            nota = teclado.nextDouble();
+            acum = acum + nota;
+            media = acum / 4;
+            teclado.nextLine();
+            System.out.println("Nome: " + nome + " Media: " + media);
+            media = 0;
+            nota = 0;
+            acum = 0;
+        }
+
+    }
+
+}
